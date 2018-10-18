2020d8c6df402ef7d2204cae5defa418371c8936
Sushant Vasishta <sushant@purposeadvisorsolutions.com>
2018-10-18 10:52:51 -0400

    placeholders

---
+++ installation/install_dev_tools.sh
@@ -0,0 +1,12 @@
+#### Install Python PIP3 ####
+sudo apt -y install python3-pip
+
+
+
+#### Install MySQL Workbench ####
+sudo apt -y install mysql-server 
+
+
+
+#### Install Django ####
+sudo apt-get install mysql-workbench
