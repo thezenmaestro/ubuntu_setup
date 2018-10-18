ab798aa4c946b3f68cd77f035f03a548c676840e
Sushant Vasishta <thezenmaestro@gmail.com>
2018-10-18 12:38:35 -0400

    added screen resolution setting commands

---
+++ configuration/configure_screen_res.sh
@@ -0,0 +1,7 @@
+#### Set Resolution ####
+cvt 2560 1440
+xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync
+xrandr --addmode Virtual1 "2560x1440_60.00"
+
+#### Add the last two lines in the ~/.bashrc or ~/.xprofile ####
+
